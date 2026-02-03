aac8697e444fdfd0b0a39d084c59140cf68cea3f
Mounikavan <136370042+Mounikavan@users.noreply.github.com>
2026-02-02 21:16:11 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  description = "Candidate name"
+  type        = string
+}
+
+variable "position_title" {
+  description = "Position being applied for"
+  type        = string
+}
